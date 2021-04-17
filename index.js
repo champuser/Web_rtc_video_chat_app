1d5e338a68aa155918eb17705b10e0e87f9693bb
champuser <ujjawalchoudhary2016@gmail.com>
2021-04-18 00:02:23 +0530

    web_rtc_video_chat_app

---
+++ index.js
@@ -0,0 +1,41 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const cors = require('cors');
+const { Socket } = require('net');
+
+const io = require('socket.io')(server,{
+    cors : {
+        origin:'*',
+        methods: ['GET','POST']
+    }
+});
+
+app.use(cors());
+
+const PORT = process.env.PORT || 4000;
+
+app.get('/',(req,res)=>{
+    res.send("server is running")
+});
+
+io.on('connection',(socket)=>{
+    // socket is used for real time data transmission that data may be  text,audio and video
+       
+    socket.emit('me',socket.id);
+
+    socket.on('disconnect',()=>{
+        socket.broadcast.emit("callended");
+    });
+
+    socket.on("calluser",({userToCall,signalData,from,name}) => {
+        io.to(userToCall).emit("calluser",{signal: signalData,from,name});
+
+    });
+
+    socket.on("answercall",(data) => {
+        io.to(data.to).emit("callaccepted",data.signal);
+
+    });
+})
+
+server.listen(PORT,()=> console.log(`server is listening on port ${PORT}`));
